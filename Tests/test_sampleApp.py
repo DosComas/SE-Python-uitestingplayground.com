18fadfa1a1887cd2c99e2f9b21231ec72971a1f1
Roberto Hurry <hurryroberto@gmail.com>
2022-12-17 11:21:01 -0300

    Sampale app change

--- Tests/test_sampleApp.py
+++ Tests/test_sampleApp.py
@@ -8,7 +8,8 @@ class TestCase():
 
     @pytest.mark.parametrize('username, password, expected_result',
                              TestData.sample_app_login_data)
-    def test_sample_app(self, init_driver, username, password, expected_result):
+    def test_sample_app(self, init_driver, username, password,
+                        expected_result):
         self.sampleApp = SampleApp(init_driver)
         text = self.sampleApp.go_and_do_login(username, password)
         assert text == expected_result
